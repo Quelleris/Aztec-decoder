f1e596c49f23ffa57e6e98736edab483acfdbcbb
Quelleris <74406524+Quelleris@users.noreply.github.com>
2023-11-13 18:07:23 +0100

    Add LoG filter and choosing denoise method functionality

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from PyQt5.QtWidgets import QApplication
 import sys
 from main_window import MainWindow
-import zxingcpp
-import cv2 as cv
 
 if __name__ == "__main__":   
     app = QApplication(sys.argv)
--- main_window.py
+++ main_window.py
@@ -4,21 +4,20 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 import cv2
 import numpy as np
-from Ui_main_window import Ui_MainWindow
+from Ui_Main_Window import Ui_MainWindow
 from dialogs.error import ErrorMessage
 import zxingcpp
-from matplotlib.image import imread
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
 
-        #UI
+        self.image_path = ""
+
+        #Set UI
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.show()
-
-        self.image_path = ""
 
         #Connect functions to buttons
         self.ui.Btn_open_image.clicked.connect(self.open_file)
@@ -36,20 +35,22 @@ class MainWindow(QMainWindow):
         self.set_image()
         
 
-    def set_image(self, ):
+    def set_image(self):
         self.pixmap = QPixmap(self.image_path)
         pixmap_resized = self.pixmap.scaled(self.ui.Lb_Image.width(), self.ui.Lb_Image.height())
         self.ui.Lb_Image.setPixmap(pixmap_resized)
 
     def decode_aztec_code(self):
-        # denoised_image = self.denoise_image_1()
-        img = cv2.imread(self.image_path)
-        found_aztec_codes = zxingcpp.read_barcodes(img)
+        if self.ui.Rbtn_odszumianie_1.isChecked():
+            denoised_image = self.denoise_image_1()
+        elif self.ui.Rbtn_odszumianie_2.isChecked():
+            denoised_image = self.denoise_image_2()
+        found_aztec_codes = zxingcpp.read_barcodes(denoised_image)
         for code in found_aztec_codes:
             self.ui.Te_decoded_text.setText("Found barcode:\n Text:    '{}'\n Format:   {}\n Position: {}".format(code.text, code.format, code.position))
         if len(found_aztec_codes) == 0:
             self.ui.Te_decoded_text.setText("Could not find any barcodes.")
-        cv2.imshow("image", img)
+        cv2.imshow("image", denoised_image)
 
     def draw_aztec_codes_in_image(self):
         image = cv2.imread(self.image_path)
@@ -78,16 +79,21 @@ class MainWindow(QMainWindow):
         cv2.imshow("img", image)
 
     def denoise_image_1(self):
-        img = cv2.imread(self.image_path, cv2.IMREAD_GRAYSCALE)
-        blur = cv2.GaussianBlur(img,(3,3),0)
-        _,th3 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-        return th3
+        img = cv2.imread(self.image_path)
+        blur_mask = np.array([[1,2, 1],[2, 4,2],[1,2, 1]])
+        laplacian_mask = np.array([[-1,-1, -1],[-1, 9,-1],[-1,-1, -1]])
+        res_step1 = cv2.GaussianBlur(img, (3,3), 0)
+        res_step2 = cv2.filter2D(src=res_step1,ddepth=-1, kernel=laplacian_mask)
+        return res_step2
     
     def denoise_image_2(self):
-        pass
+        img = cv2.imread(self.image_path)
+        res_step2 = cv2.medianBlur(img, 3)
+        return res_step2
     
     # def open_about_dialog(self):
     #     self.dialog = QDialog()
     #     self.ui = Ui_aboutDialog()
     #     self.ui.setupUi(self.dialog)
     #     self.dialog.exec_()
+
